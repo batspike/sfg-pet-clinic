9451f3ccac184732d9e980183d2087edced50ef6
Sam Lau <samlau.sam@gmail.com>
2020-07-27 18:53:12 +0800

    create interfaces for Services. closes #5

---
+++ pet-clinic-data/src/main/java/com/samcancode/sfgpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package com.samcancode.sfgpetclinic.services;
+
+import java.util.Set;
+
+import com.samcancode.sfgpetclinic.model.Pet;
+
+
+public interface PetService {
+	Pet findById(Long id);
+	Pet save(Pet pet);
+	Set<Pet> findAll();
+}
