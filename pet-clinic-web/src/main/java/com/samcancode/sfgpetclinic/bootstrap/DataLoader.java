9b6878e2d734183682c7407381e96055175c776d
Sam Lau <samlau.sam@gmail.com>
2020-07-31 15:20:43 +0800

    add PetType sample data in DataLoad.

--- pet-clinic-web/src/main/java/com/samcancode/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/samcancode/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.samcancode.sfgpetclinic.model.Owner;
+import com.samcancode.sfgpetclinic.model.PetType;
 import com.samcancode.sfgpetclinic.model.Vet;
 import com.samcancode.sfgpetclinic.services.OwnerService;
+import com.samcancode.sfgpetclinic.services.PetTypeService;
 import com.samcancode.sfgpetclinic.services.VetService;
 
 @Component
@@ -13,14 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	private final PetTypeService petTypeService;
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 
 	@Override
 	public void run(String... args) throws Exception {
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		PetType savedCatPetType = petTypeService.save(cat);
+		
+		System.out.println("PetType Loaded.");
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
