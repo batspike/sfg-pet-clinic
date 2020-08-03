efed3b55cb30e6cb3c53e22a065fe8d960f93ecc
Sam Lau <samlau.sam@gmail.com>
2020-08-03 17:36:07 +0800

    annotate BaseEntity with JPA annotations

--- pet-clinic-data/src/main/java/com/samcancode/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/samcancode/sfgpetclinic/model/BaseEntity.java
@@ -2,9 +2,17 @@ package com.samcancode.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //tells JPA to reat this as ordinary class, not entity
 public class BaseEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
